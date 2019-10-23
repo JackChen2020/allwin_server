1f66945d20f88dd672c334d416af20ebdc187222
gigahertz <jackgoodchen2019@gmail.com>
2019-10-23 13:06:34 +0800

    upd

--- apps/business/utils.py
+++ apps/business/utils.py
@@ -915,7 +915,7 @@ class CreateOrder(object):
             print(self.order.amount)
             print(type(self.order.amount))
             request_data = {
-                "amount": int(str(self.order.amount)),
+                "amount": str(int(float(self.order.amount))),
                 "orderNumber": str(self.order.ordercode),
                 "notifyUrl": url_join('/callback_api/lastpass/zhongxing_callback')
             }
