60faa10540ac4c5c46e51a6a2a49d537e0b07ea4
gigahertz <jackgoodchen2019@gmail.com>
2019-11-03 17:57:42 +0800

    upd

--- apps/lastpass/utils.py
+++ apps/lastpass/utils.py
@@ -4894,7 +4894,7 @@ class LastPass_HUIHUANG(LastPassBase):
             "params" : self.data
         }
         print(data)
-        result = request(method='POST', url=self.create_order_url, data=data,json=data ,verify=False)
+        result = request(method='POST', url=self.create_order_url, params=data ,verify=False)
         print(result.text)
         self.response = json.loads(result.content.decode('utf-8'))
 
