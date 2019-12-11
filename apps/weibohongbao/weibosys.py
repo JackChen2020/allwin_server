51283693dfc0dfe1571d7f31001613ca0ee5322b
gigahertz <jackgoodchen2019@gmail.com>
2019-12-11 15:21:50 +0800

    upd

--- apps/weibohongbao/weibosys.py
+++ apps/weibohongbao/weibosys.py
@@ -201,7 +201,7 @@ class weiboSysRun(object):
                 raise PubErrorCustom("群成员ID不正确{}".format(item))
 
         #群组长关联各个群成员
-        followClass = WeiboFollow(sessionRes=json.loads(userBossObj.session),cookieKey='.weibo.com')
+        followClass = WeiboFollow(sessionRes=json.loads(userBossObj.session))
         for item in userMemberObjs:
             followClass.follow(item.uid)
 
